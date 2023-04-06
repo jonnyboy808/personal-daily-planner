35e06a58b715d89122c5a846f41861333ca60bab
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-06 16:11:15 -0700

    corrected issue with past hours not displaying

--- script.js
+++ script.js
@@ -38,14 +38,15 @@ $(function () {
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1").val(localStorage.getItem("hour-1"));
-$("#hour-2").val(localStorage.getItem("hour-2"));
-$("#hour-3").val(localStorage.getItem("hour-3"));
-$("#hour-4").val(localStorage.getItem("hour-4"));
-$("#hour-5").val(localStorage.getItem("hour-5"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
 
 $(document).ready(function() {
 
+  $("#currentDay").text(moment().format("MMM Do, YYYY, h:MM a"));
 
 function checkHour() {
   var currentHour = moment().hour();
@@ -231,7 +232,7 @@ function checkHour() {
   })
 } checkHour();
 
-}
+})
 
 // var currentTime = dayjs().format("MMMM/DD/YYYY, h:mm:ss a");
 // $("#currentDay").text(currentTime);
