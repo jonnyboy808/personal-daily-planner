63207b5deb3e1a9f85b7d48a4e54769c9363cd99
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-06 19:01:26 -0700

    added final comments of explination

--- script.js
+++ script.js
@@ -1,74 +1,40 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
-
-
+// creates the date and time for the header in the current day id
 function updateClock (){
   var now = dayjs().format('MMM D, YYYY h:mm:ss A');
 $('#currentDay').text(now);
 }
+// allows the seconds to be updated every second
 $(document).ready(function() {
   setInterval('updateClock()', 1000);
 });
 
-
-
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1 .description").val(localStorage.getItem("hour-1"));
-$("#hour-2 .description").val(localStorage.getItem("hour-2"));
-$("#hour-3 .description").val(localStorage.getItem("hour-3"));
-$("#hour-4 .description").val(localStorage.getItem("hour-4"));
-$("#hour-5 .description").val(localStorage.getItem("hour-5"));
-
+// function to compare the class with the hour and assigns a new class
 $(document).ready(function() {
-
-  // $("#currentDay").text(moment().format("MMM Do, YYYY, h:MM a"));
-
 function checkHour() {
+  // compares current time to hour
   var currentHour = moment().hour();
-
+// compares the class number, in this case of "9" to the current hour 
   $(".9").each(function() {
+    // if current hour is greater than current class, it assigns it a "past" class
     if (9 < currentHour) {
       $(this).addClass("past");
       $(this).removeClass("present");
       $(this).removeClass("future");
     }
+    // if current hour is equal than current class, it assigns it a "present" class
     else if (9 === currentHour) {
       $(this).removeClass("past");
       $(this).addClass("present");
       $(this).removeClass("future");
     }
+    // if neither of the above apply then it assigns a future class
     else {
       $(this).removeClass("past");
       $(this).removeClass("present");
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".10").each(function() {
     if (10 < currentHour) {
       $(this).addClass("past");
@@ -86,7 +52,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".11").each(function() {
     if (11 < currentHour) {
       $(this).addClass("past");
@@ -104,7 +70,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".12").each(function() {
     if (12 < currentHour) {
       $(this).addClass("past");
@@ -122,7 +88,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".13").each(function() {
     if (13 < currentHour) {
       $(this).addClass("past");
@@ -140,7 +106,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".14").each(function() {
     if (14 < currentHour) {
       $(this).addClass("past");
@@ -158,7 +124,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".15").each(function() {
     if (9 < currentHour) {
       $(this).addClass("past");
@@ -176,7 +142,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".15").each(function() {
     if (15 < currentHour) {
       $(this).addClass("past");
@@ -194,7 +160,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".16").each(function() {
     if (16 < currentHour) {
       $(this).addClass("past");
@@ -212,7 +178,7 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-
+// steps are repeated from comment on line 15, 24, & 30
   $(".17").each(function() {
     if (17 < currentHour) {
       $(this).addClass("past");
@@ -230,34 +196,24 @@ function checkHour() {
       $(this).addClass("future");
     }
   })
-  $(".18").each(function() {
-    if (17 < currentHour) {
-      $(this).addClass("past");
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-    }
-    else if (18 === currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-      $(this).removeClass("future");
-    }
-    else {
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future");
-    }
-  })
-
 } checkHour();
-
-$(".saveBtn").on("click", function () {
+// creates a click function and links it to the saveBtn class in html
+$(".saveBtn").on("click", function (event) {
+  // prevents the default from replacing the saved text
   event.preventDefault();
-  var time = $(this).parent().attr("id");
-  var text = $(this).siblings(".description").val();
-  localStorage.setItem(time, text);
+  // variables for the time slot and text to be assigned together to save inputed items to the task class
+  var timeSlot = $(this).parent().attr("id");
+  var textSlot = $(this).siblings(".task").val();
+  // saves the inputed items to the local storage
+  localStorage.setItem(timeSlot, textSlot);
 })
-
 })
-
-// var currentTime = dayjs().format("MMMM/DD/YYYY, h:mm:ss a");
-// $("#currentDay").text(currentTime);
+// creates pulls from the storage when the tasks are saved and assigns them to their respected hour slots
+$("#hour-9 .task").val(localStorage.getItem("hour-9"));
+$("#hour-10 .task").val(localStorage.getItem("hour-10"));
+$("#hour-11 .task").val(localStorage.getItem("hour-12"));
+$("#hour-1 .task").val(localStorage.getItem("hour-1"));
+$("#hour-2 .task").val(localStorage.getItem("hour-2"));
+$("#hour-3 .task").val(localStorage.getItem("hour-3"));
+$("#hour-4 .task").val(localStorage.getItem("hour-4"));
+$("#hour-5 .task").val(localStorage.getItem("hour-5"));
